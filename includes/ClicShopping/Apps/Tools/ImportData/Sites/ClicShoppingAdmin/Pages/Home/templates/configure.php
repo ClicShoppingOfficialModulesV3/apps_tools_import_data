5f4732c4a257dffaf840dc700c6708f5a50faea8
ClicShopping <Yae8mr2o*>
2021-01-28 16:00:20 +0100

    update template

--- includes/ClicShopping/Apps/Tools/ImportData/Sites/ClicShoppingAdmin/Pages/Home/templates/configure.php
+++ includes/ClicShopping/Apps/Tools/ImportData/Sites/ClicShoppingAdmin/Pages/Home/templates/configure.php
@@ -97,7 +97,7 @@
             echo HTML::button($CLICSHOPPING_ImportData->getDef('button_save'), null, null, 'success');
 
             if ($CLICSHOPPING_ImportData->getConfigModuleInfo($current_module, 'is_uninstallable') === true) {
-              echo '<span class="float-md-right">' . HTML::button($CLICSHOPPING_ImportData->getDef('button_dialog_uninstall'), null, '#', 'warning', ['params' => 'data-toggle="modal" data-target="#ppUninstallModal"']) . '</span>';
+              echo '<span class="float-end">' . HTML::button($CLICSHOPPING_ImportData->getDef('button_dialog_uninstall'), null, '#', 'warning', ['params' => 'data-toggle="modal" data-target="#ppUninstallModal"']) . '</span>';
             }
           ?>
         </div>
